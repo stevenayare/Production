52bf99d9b05a6332d159946816fea029bd2eed2a
Ved <64102456+stevenayare@users.noreply.github.com>
2023-10-27 23:25:59 +0530

    Update settings.py

--- WebCNN/settings.py
+++ WebCNN/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = 'static/'
 
 #Added Manually
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
+    BASE_DIR / "static"
+    
 ]
 
 STATIC_ROOT = '/var/www/static/'
